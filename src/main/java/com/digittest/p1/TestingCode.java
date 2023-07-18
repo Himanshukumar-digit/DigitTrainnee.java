bccd24452f0b73de5e262fe378f4e6b3b4f73b57
Himanshu.Kumar01 <Himanshu.Kumar01@LDIGITBGL-L0272.godigit.corp>
2023-07-18 12:31:53 +0530

    this is my first commit

--- src/main/java/com/digittest/p1/TestingCode.java
+++ src/main/java/com/digittest/p1/TestingCode.java
@@ -1,5 +1,10 @@
 package com.digittest.p1;
 
 public class TestingCode {
+	public static void main(String[] args) {
+		System.out.println("Ram");
+		System.out.println("Ram");
+		System.out.println("Ram");
+	}
 
 }
